1bcc0e876816685190aaf9fb0f2dbc1a2e7870c2
Luke Metz <luke.metz@students.olin.edu>
2013-09-24 16:09:13 -0400

    added calendar

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
 
     $routeProvider.when('/inspiration/codeAcademy', {templateUrl: 'partials/inspiration/codeAcademy.html', controller: 'EmptyController'});
     $routeProvider.when('/inspiration/maps', {templateUrl: 'partials/inspiration/maps.html', controller: 'EmptyController'});
+    $routeProvider.when('/inspiration/calendar', {templateUrl: 'partials/inspiration/calendar.html', controller: 'EmptyController'});
     $routeProvider.when('/inspiration/dayOne', {templateUrl: 'partials/inspiration/dayOne.html', controller: 'EmptyController'});
 
     $routeProvider.when('/projectBrief', {templateUrl: 'partials/projectBrief.html', controller: 'EmptyController'});
