545ced77324c94bb56bd69bbe504424d87a0f730
Luke Metz <luke.metz@students.olin.edu>
2013-10-01 17:11:41 -0400

    added table

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
 
     $routeProvider.when('/projectBrief', {templateUrl: 'partials/projectBrief.html', controller: 'EmptyController'});
     $routeProvider.when('/needs', {templateUrl: 'partials/needs.html', controller: 'EmptyController'});
+    $routeProvider.when('/breakdown', {templateUrl: 'partials/divisionOfLabor.html', controller: 'EmptyController'});
     $routeProvider.when('/', {templateUrl: 'partials/index.html', controller: 'EmptyController'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
